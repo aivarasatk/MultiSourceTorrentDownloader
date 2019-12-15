530c7a86d9f633980da37ad41e23edbe9a6f43f6
aivarasatk <42089270+aivarasatk@users.noreply.github.com>
2019-12-15 22:42:20 +0200

    Added html visualizer for torrent descriptions. Added references that allow webbrowser in dialoghost

---
+++ MultiSourceTorrentDownloader/Behaviors/BindHtmlBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace MultiSourceTorrentDownloader.Behaviors
+{
+    public class BindHtmlBehavior : Behavior<WebBrowser>
+    {
+        public static readonly DependencyProperty HtmlProperty =
+                DependencyProperty.Register("Html", typeof(string), typeof(BindHtmlBehavior));
+
+        public string Html
+        {
+            get { return (string)GetValue(HtmlProperty); }
+            set { SetValue(HtmlProperty, value); }
+        }
+
+        protected override void OnAttached()
+        {
+            AssociatedObject.Loaded += Loaded;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.Loaded -= Loaded;
+        }
+
+        private void Loaded(object sender, RoutedEventArgs e)
+        {
+            AssociatedObject.NavigateToString(Html ?? string.Empty);
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # MultiSourceTorrentDownloader
 An app that gets torrents from multiple sites (like thepiratebay, 133tx.to) and shows them in one place with the ability to download
 
+## Compilation
+Add two references in DwayneNeedInterop folder (this fixes the webbrowser being visible in the dialog host)
+
 ## Contributing
 [MIT license](license.txt)
