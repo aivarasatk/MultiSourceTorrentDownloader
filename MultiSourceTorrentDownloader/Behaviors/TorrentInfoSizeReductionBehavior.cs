159e24f8ed1ebe9167750a69dce084b182328201
aivarasatk <42089270+aivarasatk@users.noreply.github.com>
2019-12-31 12:35:20 +0200

    Torrent info dialog opens with similar size as the main window

---
+++ MultiSourceTorrentDownloader/Behaviors/TorrentInfoSizeReductionBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace MultiSourceTorrentDownloader.Behaviors
+{
+    public class TorrentInfoSizeReductionBehavior : Behavior<UserControl>
+    {
+        private bool _sizeChangeInProgress = true;
+        protected override void OnAttached()
+        {
+            AssociatedObject.SizeChanged += SizeChanged;
+        }
+
+        private void SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            _sizeChangeInProgress = !_sizeChangeInProgress;
+            if (_sizeChangeInProgress) return;
+            
+            AssociatedObject.Width = AssociatedObject.Width * 0.9;
+            AssociatedObject.Height = AssociatedObject.Height * 0.8;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.SizeChanged -= SizeChanged;
+        }
+    }
+}
--- MultiSourceTorrentDownloader/Views/MainWindow.xaml.cs
+++ MultiSourceTorrentDownloader/Views/MainWindow.xaml.cs
@@ -21,7 +21,7 @@ namespace MultiSourceTorrentDownloader.Views
             var viewModel = kernel.Get<MainViewModel>();
 
             DataContext = viewModel.Model;
-            
+
         }
     }
 }
