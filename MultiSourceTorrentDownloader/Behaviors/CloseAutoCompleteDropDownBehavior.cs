76751740e5b5bfbf501f71b1fc771a8bfe05f044
Aivaras <AIAT@danskebank.lt>
2021-03-27 21:34:37 +0200

    Added search autocomplete box from Synfusion

---
+++ MultiSourceTorrentDownloader/Behaviors/CloseAutoCompleteDropDownBehavior.cs
@@ -0,0 +1,32 @@
+﻿using Syncfusion.Windows.Controls.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Interactivity;
+
+namespace MultiSourceTorrentDownloader.Behaviors
+{
+    public class CloseAutoCompleteDropDownBehavior : Behavior<SfTextBoxExt>
+    {
+        protected override void OnAttached()
+        {
+            AssociatedObject.LostKeyboardFocus += OnLostFocus;
+
+            base.OnAttached();
+        }
+
+        private void OnLostFocus(object sender, System.Windows.RoutedEventArgs e)
+        {
+            AssociatedObject.IsSuggestionOpen = false;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.LostKeyboardFocus -= OnLostFocus;
+
+            base.OnDetaching();
+        }
+    }
+}
---
+++ MultiSourceTorrentDownloader/Interfaces/IAutoCompleteService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace MultiSourceTorrentDownloader.Interfaces
+{
+    public interface IAutoCompleteService
+    {
+        IReadOnlyCollection<string> AutoCompletes { get; }
+
+        void Init(IEnumerable<string> autoCompletes);
+        void TryAddAutoCompleteEntry(string entry);
+    }
+}
